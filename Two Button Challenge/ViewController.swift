86533c4cdb2c48598e9fb7459967d3f9343806d4
Glenda Mullan <glenda.mullan@gmail.com>
2022-09-28 15:06:13 +1000

    A few minor changes and //comments with descriptions

--- Two Button Challenge/ViewController.swift
+++ Two Button Challenge/ViewController.swift
@@ -19,12 +19,15 @@ class ViewController: UIViewController {
         messageLabel.text = "  You are Awesome!"
         messageLabel.textColor = UIColor.red
         messageLabel.textAlignment = NSTextAlignment.left
+        messageLabel.backgroundColor = .clear
     }
     @IBAction func showAlternativeMessage(_ sender: UIButton) {
         print("Show Alternative Message button pressed")
         messageLabel.text = "You are Great!  "
-        messageLabel.textColor = UIColor.blue
-        messageLabel.textAlignment = NSTextAlignment.right
+        // when setting text attribute, after the = you can either write full description (as shown above, if known) or try typing a . and a list of options for that attribute will come up
+        messageLabel.textColor = .blue
+        messageLabel.textAlignment = .right
+        messageLabel.backgroundColor = .lightGray  // need to state background colour in both buttons otherwise the the last setting is used
     }
     
 
